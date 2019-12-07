fff768cc8ee61d047c21a9b5d5673e762021cab8
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-12-07 00:08:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,10 +5,15 @@ def prime?(int)
   #binding.pry
   #if int > 0
     #return true
-  elsif int < 0
+  # elsif int < 0
     
-    return true
-  else
-    puts "#{int}, Be sure to account for negative numbers!"
-  end 
+  #   return true
+  # else
+  #   puts "#{int}, Be sure to account for negative numbers!"
+  # end 
+  if int < 0 || int == 0 || int == 1
+    return false
+  else 
+    
+  end
 end
